e9b56d2a7f9dc7f4fdda8102a51294fa8fa274d3
Dheeraj Yalamanchili <dhee9000@gmail.com>
2020-06-14 16:35:18 -0500

    added agora-access-token

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,32 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+// Agora Functions
+const {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token');
+
+const AppID = 'b1350a7f93bc4fe18bdc2e3b3a8952e1';
+const AppCertificate = '9b06228fbb2f4248a6c6ad5bd8badd05';
+const Role = RtcRole.PUBLISHER;
+
+const EXPIRY_IN_SECONDS = 240
+
+generateRTCToken = function generateRTCToken(uid, channel){
+    const currentTimestamp = Math.floor(Date.now() / 1000)
+    const expiryTime = currentTimestamp + EXPIRY_IN_SECONDS
+    const token = RtcTokenBuilder.buildTokenWithAccount(AppID, AppCertificate, channel, uid, Role, 0);
+    console.log(`UID: ${uid}, Channel: ${channel}, Token: ${token}`);
+    return token;
+}
+
+exports.myFunction = functions.firestore.document('pairingPool/{id}').onCreate((change, context) => {
+    
+    // Get the request params
+    let pairRequest = change.data();
+
+    // Search for a matching profile in the pool
+    
+
+});
+
